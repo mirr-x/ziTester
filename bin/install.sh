1eb860b2ebec17d4722b7f55120459dd7e937df1
mirr-x <amir.exe@icloud.com>
2024-08-21 19:06:42 +0100

    z

--- bin/install.sh
+++ bin/install.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-# Define color codes
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
-
-# Define the path to the project and Makefile
-TARGET_DIR="/home/$USER/ziTester"
-MAKEFILE_PATH="$TARGET_DIR/bin/Makefile"
-
-# Check if the directory exists
-if [ -d "$TARGET_DIR" ]; then
-    rm -rf "$TARGET_DIR"
-fi
+# Colors
+YELLOW='\033[33m'
+RED='\033[31m'
+GREEN='\033[32m'
+NC='\033[0m'
 
 # Clone the repository
-echo "${YELLOW}Building the Test Framework...${NC}\n"
+echo -e "${YELLOW}Building the Test Framework...${NC}\n"
 git clone https://github.com/mirr-x/ziTester "$TARGET_DIR"
 
 sleep 1
@@ -28,7 +19,7 @@ cd "$TARGET_DIR/bin"
 if [ -f "Makefile" ]; then
     make
 else
-    echo "${RED}No Makefile found in $TARGET_DIR. Skipping build step.${NC}"
+    echo -e "${RED}No Makefile found in $TARGET_DIR. Skipping build step.${NC}"
 fi
 
 sleep 1
@@ -49,7 +40,7 @@ sleep 1
 
 # Confirm completion
 if [ -d "$TARGET_DIR" ]; then
-    echo "${GREEN}Test Framework successfully built and installed.${NC}\n"
+    echo -e "${GREEN}Test Framework successfully built and installed.${NC}\n"
 else
-    echo "${RED}Failed to clone the repository.${NC}"
+    echo -e "${RED}Failed to clone the repository.${NC}"
 fi
