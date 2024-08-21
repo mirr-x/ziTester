f925a3d94850bfdf948f19754cd3e78a4dbdf7cf
mirr-x <amir.exe@icloud.com>
2024-08-21 19:15:03 +0100

    z

--- bin/install.sh
+++ bin/install.sh
@@ -19,25 +19,25 @@ if [ -d "$TARGET_DIR" ]; then
 fi
 
 # Clone the repository
-echo -e "${YELLOW}Building the Test Framework...${NC}"
+echo "${YELLOW}Building the Test Framework...${NC}"
 if ! git clone https://github.com/mirr-x/ziTester "$TARGET_DIR"; then
-    echo -e "${RED}Failed to clone the repository.${NC}"
+    echo "${RED}Failed to clone the repository.${NC}"
     exit 1
 fi
 
 sleep 1
 
 # Navigate to the directory containing the Makefile
-cd "$TARGET_DIR/bin" || { echo -e "${RED}Failed to navigate to $TARGET_DIR/bin.${NC}"; exit 1; }
+cd "$TARGET_DIR/bin" || { echo "${RED}Failed to navigate to $TARGET_DIR/bin.${NC}"; exit 1; }
 
 # Check if Makefile exists and build the project
 if [ -f "Makefile" ]; then
     if ! make; then
-        echo -e "${RED}Build failed.${NC}"
+        echo "${RED}Build failed.${NC}"
         exit 1
     fi
 else
-    echo -e "${RED}No Makefile found in $TARGET_DIR. Skipping build step.${NC}"
+    echo "${RED}No Makefile found in $TARGET_DIR. Skipping build step.${NC}"
     exit 1
 fi
 
@@ -45,7 +45,7 @@ sleep 1
 
 # Check if the binary exists
 if [ ! -f "$EXE" ]; then
-    echo -e "${RED}Executable not found: $EXE.${NC}"
+    echo "${RED}Executable not found: $EXE.${NC}"
     exit 1
 fi
 
@@ -56,7 +56,7 @@ fi
 
 # Move the binary to the bin directory
 if ! mv "$EXE" "$BIN_DIR"; then
-    echo -e "${RED}Failed to move the binary to $BIN_DIR.${NC}"
+    echo "${RED}Failed to move the binary to $BIN_DIR.${NC}"
     exit 1
 fi
 
@@ -64,8 +64,8 @@ sleep 1
 
 # Confirm completion
 if [ -d "$TARGET_DIR" ]; then
-    echo -e "${GREEN}Test Framework successfully built and installed.${NC}"
+    echo "${GREEN}Test Framework successfully built and installed.${NC}"
 else
-    echo -e "${RED}Failed to clone the repository.${NC}"
+    echo "${RED}Failed to clone the repository.${NC}"
     exit 1
 fi
